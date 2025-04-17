27adb01e6d5b065ecc4ba5963455ef9ed1b674b5
bodhichristian <clavelle23@gmail.com>
2025-04-17 09:11:19 -0400

    add auth tests

--- auth-testing/test_auth.py
+++ auth-testing/test_auth.py
@@ -18,6 +18,24 @@ def test_create_account_with_existing_username():
 
     assert not auth.create_account(username, password)
 
+def test_create_account_with_blank_username():
+    username = ''
+    password = 'password'
+
+    users = {}
+    users[username] = auth.create_account(username, password)
+
+    assert not auth.create_account(username, password)
+
+def test_create_account_with_blank_password():
+    username = 'userdude'
+    password = ''
+
+    users = {}
+    users[username] = auth.create_account(username, password)
+
+    assert not auth.create_account(username, password)
+
 def test_login_with_correct_password():
     username = 'newuser'
     password = 'supersecure'
@@ -35,3 +53,27 @@ def test_login_with_incorrect_password():
     users[username] = auth.create_account(username, password)
 
     assert not auth.login(username, 'random')
+
+def test_login_with_nonexistent_username():
+    username = 'brandnewuser'
+    password = 'security'
+
+    users = {}
+    users[username] = auth.create_account(username, password)
+
+    assert not auth.login(username, 'random')
+
+def test_password_is_hashed():
+    raw = 'password'
+    hashed = auth._hash_password(raw)
+    assert hashed != raw
+    assert len(hashed) == 64 # SHA-256 length
+
+def test_password_case_sensitivity():
+    username = 'Chad'
+    password ='Password'
+
+    users = {}
+    users[username] = auth.create_account(username, password)
+
+    assert not auth.login(username, password.lower())
