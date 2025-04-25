e6f256c1be18fc33e83b818c307c59e2cfddf436
bodhichristian <clavelle23@gmail.com>
2025-04-25 07:28:59 -0400

    enumerate user types, update user model

--- auth-testing/models/user.py
+++ auth-testing/models/user.py
@@ -1,10 +1,12 @@
 import uuid
+from enum import Enum
 from datetime import date
 
 class User:
-    def __init__(self, username: str, password_hash: str, id: str=None, created_on: date=None):
+    def __init__(self, username: str, password_hash: str, user_type=UserType.STANDARD, id: str=None, created_on: date=None):
         self.username = username
         self.password_hash = password_hash
+        self.user_type = user_type
         self.id = id or str(uuid.uuid4())
         self.created_on = created_on or date.today()
 
@@ -24,3 +26,7 @@ class User:
             id=data['id'],
             created_on=date.fromisoformat(data.get('created_on'))
         )
+
+class UserType(Enum):
+    ADMIN = 'admin'
+    STANDARD = 'standard'
