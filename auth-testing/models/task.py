b4798d8c2e763237d0141e47c873e920d6dd947a
bodhichristian <clavelle23@gmail.com>
2025-04-27 14:04:11 -0400

    remove UserType enum

--- auth-testing/models/task.py
+++ auth-testing/models/task.py
@@ -2,15 +2,10 @@ import uuid
 from enum import Enum
 from datetime import date
 
-class TaskType(Enum):
-    REQUEST = 'request'
-    TODO = 'todo'
-
 class Task:
-    def __init__(self, title: str, description: str, task_type: TaskType, created_by: User.id, assigned_to: User.id=None, id: str=None, created_on: date=None, due_date: date=None):
+    def __init__(self, title: str, description: str, created_by: User.id, assigned_to: User.id=None, id: str=None, created_on: date=None, due_date: date=None):
         self.title = title
         self.description = description
-        self.task_type = task_type
         self.created_by = created_by
         self.assigned_to = assigned_to
         self.id = id or str(uuid.uuid4())
