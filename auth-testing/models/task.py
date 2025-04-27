3acc55b982c091d164d21e49fdf43c8dc474cecc
bodhichristian <clavelle23@gmail.com>
2025-04-27 14:03:38 -0400

    update Task type to include a created_by and assigned_to property

--- auth-testing/models/task.py
+++ auth-testing/models/task.py
@@ -7,10 +7,12 @@ class TaskType(Enum):
     TODO = 'todo'
 
 class Task:
-    def __init__(self, title: str, description: str, task_type: TaskType, id: str=None, created_on: date=None, due_date: date=None):
+    def __init__(self, title: str, description: str, task_type: TaskType, created_by: User.id, assigned_to: User.id=None, id: str=None, created_on: date=None, due_date: date=None):
         self.title = title
         self.description = description
         self.task_type = task_type
+        self.created_by = created_by
+        self.assigned_to = assigned_to
         self.id = id or str(uuid.uuid4())
         self.created_on = created_on or date.today()
         self.due_date = due_date
